689076f930ff9a6e0cf7823cfbd4a9888bcc053a
José João Ferreira <jjasferreira@gmail.com>
2022-06-05 18:59:15 +0100

    Code Takuzu's methods, new Board methods, new test example passed

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # takuzu-py
 
-> Projeto para a disciplina de Inteligência Artificial (LEIC-A @ IST 21/22)
->
-> > Um programa em Python 3.8 que resolve o problema Takuzu usando técnicas A.I. de busca
+Projeto para a disciplina de Inteligência Artificial (LEIC-A @ IST 21/22)
+Um programa em Python 3.8 que resolve o problema _Takuzu_ usando técnicas "A.I." de busca
+
+## Dúvidas
+
+- O que é o Node, nos utils.py? É uma abstração do TazukuState? Onde é que implementamos as funções expand.
+- É suposto implementarmos ideias dos algoritmos de CSPs (Problemas de Satisfação de Restrições)?
+- podemos alterar retornar adjacentes para retornar os dois adjacentes
 
 ## TODO List:
 
     Lista de recursos a implementar
 
-- [ ] asd
-- [ ] asd
+- [ ] apply action
+- [ ] goal state
 - [ ] asd
 
 ---
--- takuzu.py
+++ takuzu.py
@@ -35,8 +35,8 @@ class TakuzuState:
 
 class Board:
     """Representação interna de um tabuleiro de Takuzu."""
-    def __init__(self, mat, dim):
-        self.state = np.array(mat)
+    def __init__(self, array, dim):
+        self.state = array
         self.dim = dim
         
     def __repr__(self):
@@ -47,6 +47,12 @@ class Board:
             res += "\n"
         return res
     
+    def get_row(self, row: int) -> tuple:
+        return tuple(self.state[row, :self.dim])
+
+    def get_column(self, column: int) -> tuple:
+        return tuple(self.state[:self.dim, column])
+
     def get_number(self, row: int, col: int) -> int:
         """Devolve o valor na respetiva posição do tabuleiro."""
         return self.state[row, col]
@@ -74,41 +80,108 @@ class Board:
         for foo in range(dim):
             # IDK: Isto faz split todas as iterações?
             mat.append([int(i) for i in input().split()])
-        return Board(mat, dim)
-        
+        return Board(np.array(mat), dim)
+
+    def apply_action(self, action):
+        array = self.state
+        array[action[0]][action[1]] = action[2]
+        return Board(array, self.dim)
+
     # TODO: outros metodos da classe
 
 
 class Takuzu(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
-        # TODO
+        # NEW:
+        self.current = board
         pass
 
     def actions(self, state: TakuzuState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
-        # TODO
-        pass
+        res = []
+        board = state.board
+        for i in range (board.dim):
+            for j in range(board.dim):
+                if board.get_number(i, j) == 2:
+                    h = set()
+                    hn = board.adjacent_horizontal_numbers(i, j)
+                    if (hn == (1, 1)):
+                        h.add(0)
+                    elif (hn == (0,0)):
+                        h.add(1)
+                    else:
+                        h.update([0,1])
+                    v = set()
+                    vn = board.adjacent_vertical_numbers(i, j)
+                    if (vn == (1, 1)):
+                        v.add(0)
+                    elif (vn == (0,0)):
+                        v.add(1)
+                    else:
+                        v.update([0,1])
+                    s = h.intersection(v)
+                    if (len(s) != 0):
+                        for k in s:
+                            res.append([i, j, k])
+        return res
 
     def result(self, state: TakuzuState, action):
         """Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        # TODO
-        pass
+        board = state.board.apply_action(action)
+        return TakuzuState(board)
 
     def goal_test(self, state: TakuzuState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
-        # TODO
-        pass
+        
+        board = TakuzuState.board
+        rows = set()
+        columns = set()
+        dim = board.dim
+        even = (dim % 2 == 0)
+
+        def valid_section(section):
+            """Decide se uma linha ou coluna de um tabuleiro é válida"""
+            previous = None
+            # Número de jogadas sucessivas
+            state = 0
+            sum = 0
+            for i in range(dim):
+                val = section[i]
+                if val == 2:
+                    return False
+                sum += -1 if (val == 1) else -1
+                if val != previous:
+                    state = 1
+                else:
+                    if state == 2:
+                        return False
+                    state += 1
+                previous = val
+            return True
+
+        for i in range(dim):
+            r = board.get_row(i)
+            c = board.get_column(i)
+            if r in rows or c in columns:
+                return False
+            else:
+                if not valid_section(r) or not valid_section(c):
+                    return False
+                rows.add(r)
+                columns.add(c)
+        return True
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
         # TODO
+        # J: aceder ao node.state para ver o estado
         pass
 
     # TODO: outros metodos da classe
@@ -121,6 +194,7 @@ if __name__ == "__main__":  # Função main
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
 
+    """
     # Exemplo 1:
     board = Board.parse_instance_from_stdin()
     print("Initial:\n", board, sep="")
@@ -128,4 +202,13 @@ if __name__ == "__main__":  # Função main
     print(board.adjacent_horizontal_numbers(3,3))
     print(board.adjacent_vertical_numbers(1,1))
     print(board.adjacent_horizontal_numbers(1,1))
-    pass
+    """
+
+    # Exemplo 2:
+    board = Board.parse_instance_from_stdin()
+    print("Initial:\n", board, sep="")
+    problem = Takuzu(board)
+    initial_state = TakuzuState(board)
+    print(initial_state.board.get_number(2, 2))
+    result_state = problem.result(initial_state, (2, 2, 1))
+    print(result_state.board.get_number(2, 2))
